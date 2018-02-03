352cbb7e8a9ea5a2c53382841c0813f53af88a9d
Patrick <wxmgcs@sina.cn>
2018-02-03 20:14:11 +0800

    init

--- src/cn/diyai/datastructures/recursion/Recursion.java
+++ src/cn/diyai/datastructures/recursion/Recursion.java
@@ -1,4 +1,21 @@
 package cn.diyai.datastructures.recursion;
 
+/**
+ * 递归
+ * @author wangxiaomin
+ *
+ */
 public class Recursion {
+    Recursion(){
+    }
+
+    public int fact(int n){
+        if (n== 1){
+            return 1;
+        }else  if(n==0){
+            return 1;
+        }else{
+            return n * fact(n-1);
+        }
+    }
 }
--- src/cn/diyai/datastructures/recursion/TestRecursion.java
+++ src/cn/diyai/datastructures/recursion/TestRecursion.java
@@ -1,4 +1,10 @@
 package cn.diyai.datastructures.recursion;
 
 public class TestRecursion {
+    public static void main(String[] args){
+        Recursion recursion = new Recursion();
+        System.out.print(recursion.fact(3));
+
+
+    }
 }
