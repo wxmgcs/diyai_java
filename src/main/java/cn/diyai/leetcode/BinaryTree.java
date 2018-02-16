0af46584d44bbf0169776993e90cc75a67b0f542
Patrick <wxmgcs@sina.cn>
2018-02-16 17:46:14 +0800

    递归实现 获取二叉树深度

--- src/main/java/cn/diyai/leetcode/BinaryTree.java
+++ src/main/java/cn/diyai/leetcode/BinaryTree.java
@@ -7,48 +7,64 @@ import static java.lang.System.out;
 public class BinaryTree {
     /**
      * 递归实现 获取二叉树深度
+     *
      * @param root
      * @return
      */
     public int minDepth(TreeNode root) {
 
-        if(root == null) return 0;
-        if(root.left == null && root.right == null) return 1;
-        else if(root.left != null && root.right == null) return minDepth(root.left) + 1;
-        else if(root.left == null && root.right != null) return minDepth(root.right) + 1;
-        else  return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+        if (root == null) {
+            return 0;
+        }
+
+        if (root.left == null && root.right == null) {//如果只有根节点，则返回1
+            return 1;
+        } else if (root.left != null && root.right == null) {//若左子树为空，则返回右子树的最小深度+1
+            return minDepth(root.left) + 1;
+        } else if (root.left == null && root.right != null) {// 若右子树为空，则返回左子树的最小深度+1
+            return minDepth(root.right) + 1;
+        } else {
+            return Math.min(minDepth(root.left), minDepth(root.right)) + 1; // 左右子树都不为空时，取较小值
+        }
 
     }
 
-
+    /**
+     * //采用广度优先搜索，或者层序遍历，找到的第一个叶节点的深度即是最浅。
+     *
+     * @param root
+     * @return
+     */
     public int minDepth1(TreeNode root) {
 
-        if(root == null) return 0;
+        if (root == null) return 0;
         LinkedList<TreeNode> queue = new LinkedList<TreeNode>();
         queue.add(root);
+
         int level = 0;
-        while(!queue.isEmpty()) {
+        while (!queue.isEmpty()) {
             int levelLen = queue.size();
-            level ++;
+            level++;
             for (int i = 0; i < levelLen; i++) {
                 TreeNode node = queue.removeFirst();
-                if(node.left == null && node.right == null) return level;
-                if(node.left != null) queue.add(node.left);
-                if(node.right != null) queue.add(node.right);
+                if (node.left == null && node.right == null) return level;
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
             }
         }
         return level;
     }
 
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
         BinaryTree binaryTree = new BinaryTree();
-        TreeNode rootNode = new  TreeNode(1);
+        TreeNode rootNode = new TreeNode(1);
         TreeNode leftNode = new TreeNode(2);
         TreeNode rightNode = new TreeNode(3);
         rootNode.left = leftNode;
         rootNode.right = rightNode;
-        System.out.println(binaryTree.minDepth(rootNode));
+        rightNode.left = new TreeNode(4);
+        System.out.println(String.format("%s %s", "最小深度为", binaryTree.minDepth(rootNode)));
+        System.out.println(String.format("%s %s", "最小深度为", binaryTree.minDepth1(rootNode)));
 
     }
 
