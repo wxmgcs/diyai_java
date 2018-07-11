a74aea2a7c5c459ae3ffef00a1d224fe5006642e
Patrick <wxmgcs@sina.cn>
2018-07-11 19:31:44 +0800

    线程状态

---
+++ src/main/java/cn/diyai/java_base/thread_state/MyThread.java
@@ -0,0 +1,13 @@
+package cn.diyai.java_base.thread_state;
+
+public class MyThread extends Thread{
+    public MyThread() {
+        System.out.println("构造方法中的状态:"+Thread.currentThread().getState());
+    }
+
+
+    @Override
+    public void run() {
+        System.out.println("run方法中的状态:"+Thread.currentThread().getState());
+    }
+}
